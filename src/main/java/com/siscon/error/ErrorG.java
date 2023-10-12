ea9c821d12e888006903fa9193a3a89516d283f4
Lawrence-AIP <liuch@allinfinance.com>
2023-10-12 17:35:52 +0800

    报错加密完成

---
+++ src/main/java/com/siscon/error/ErrorG.java
@@ -0,0 +1,37 @@
+package com.siscon.error;
+
+import cn.hutool.crypto.SecureUtil;
+import cn.hutool.crypto.symmetric.AES;
+
+/**
+ * Created by IntelliJ IDEA.
+ * 1.输入报错信息msg,转化为对应的加密后的错误信息
+ * 2.输入加密后的错误信息,转化为对应的解密后的错误信息msg
+ * https://blog.csdn.net/qq_22076345/article/details/108632057
+ * https://juejin.cn/post/7028204583264632840
+ */
+public class ErrorG {
+
+    /* 随机生成密钥
+    byte[] key = SecureUtil.generateKey(SymmetricAlgorithm.AES.getValue()).getEncoded();*/
+
+    private final byte[] key = {7, 119, 83, 32, -5, 32, -47, 12, 89, 97, 105, 118, 31, 19, -26, 111};
+
+    public String generateMsg(String msg) {
+        // 构建
+        AES aes = SecureUtil.aes(key);
+        // 加密
+        String encrypt = aes.encryptBase64(msg);
+
+        return encrypt;
+    }
+
+    public String solveMsg(String msg) {
+
+        AES aes = SecureUtil.aes(key);
+
+        String decryptStr = aes.decryptStr(msg);
+
+        return decryptStr;
+    }
+}
---
+++ src/main/java/com/siscon/test/pathTest.java
@@ -0,0 +1,9 @@
+package com.siscon.test;
+
+import com.siscon.config.KeyConfig;
+
+public class pathTest {
+    public static void main(String[] args) {
+        System.out.println(new KeyConfig().BASE_FILE_PATH);
+    }
+}
