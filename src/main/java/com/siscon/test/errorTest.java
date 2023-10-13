9962ffe0dc64c81fcc56132477cd8883d38294a2
Lawrence-AIP <liuch@allinfinance.com>
2023-10-13 17:20:07 +0800

    newest

--- src/main/java/com/siscon/test/errorTest.java
+++ src/main/java/com/siscon/test/errorTest.java
@@ -5,11 +5,11 @@ import com.siscon.error.ErrorG;
 
 public class errorTest {
     public static void main(String[] args) {
-        // WEIahTgKvcWMrQf1c+MKRLd24Dt7CXzgVTdDeBRc/Y6dSVUYihKeusQQ9C10CHUr
-        System.out.println(new ErrorG().generateMsg("Hello sisconsavior,nice to meet you!"));
+
+        System.out.println(new ErrorG().generateMsg("Hello sisconsavior,nice to meet you!\n55"));
         System.out.println(new ErrorG().generateMsg("Hello w**orld!:cwei看美剧"));
 
-        String x = "WEIahTgKvcWMrQf1c+MKRLd24Dt7CXzgVTdDeBRc/Y6dSVUYihKeusQQ9C10CHUr";
+        String x = "WEIahTgKvcWMrQf1c+MKRLd24Dt7CXzgVTdDeBRc/Y4CQjY1OrK0ccprwQBpaFY5";
         String x2 = "E0GVGBbC+/AJkWOCBpLq3BT9liAqjY7pCEKQdrrNEZA=";
         System.out.println(new ErrorG().solveMsg(x));
     }
